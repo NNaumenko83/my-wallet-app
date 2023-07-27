0d9060292295458e13db68e55aa295c3148aabd6
Nikolay Naumenko <nnv2006@ukr.net>
2023-07-27 12:29:15 +0300

    Test new

--- src/hooks/useEthereumAccount.js
+++ src/hooks/useEthereumAccount.js
@@ -1,6 +1,75 @@
+// // import { ethers } from "ethers";
+// // import { useState } from "react";
+// // import { MetaMaskSDK } from "@metamask/sdk";
+
+// // import { formatAddress } from "../helpers/formatAddress";
+
+// // // DEEPL LNK
+// // // https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/
+
+// // export function useEthereumAccount() {
+// //   const [balance, setBalance] = useState(null);
+// //   const [walletAddress, setWalletAddress] = useState("");
+// //   const [signer, setSigner] = useState(null);
+// //   const [provider, setProvider] = useState();
+
+// //   async function requestAccount() {
+// //     console.log("Hello");
+// //     console.log(MetaMaskSDK);
+// //     const sdk = new MetaMaskSDK({
+// //       openDeeplink:
+// //         "https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/",
+// //     });
+
+// //     const ethereum = sdk.getProvider();
+
+// //     const test = new ethers.providers.WebBrowserProvider3Provider(ethereum);
+// //     console.log("test:", test);
+
+// //     if (window.ethereum) {
+// //       // console.log("window.ethereum):", window.ethereum);
+// //       // console.log("test:", test);
+// //       const provider = new ethers.BrowserProvider(window.ethereum);
+
+// //       const signer = await provider.getSigner();
+
+// //       const balance = await provider.getBalance(signer.address);
+
+// //       const formatted = formatAddress(signer.address);
+
+// //       setBalance(ethers.formatEther(balance));
+// //       setWalletAddress(formatted);
+// //       setSigner(signer);
+// //       setProvider(provider);
+// //     } else {
+// //       alert("No crypto wallet found. Please install it.");
+// //     }
+// //   }
+
+// //   async function transferTokens(receiverAddress, transferAmount) {
+// //     const tx = await signer.sendTransaction({
+// //       to: receiverAddress,
+// //       value: ethers.parseEther(transferAmount),
+// //     });
+
+// //     await tx.wait();
+
+// //     const balance = await provider.getBalance(signer.address);
+
+// //     setBalance(ethers.formatEther(balance));
+// //   }
+
+// //   return {
+// //     balance,
+// //     walletAddress,
+// //     requestAccount,
+// //     transferTokens,
+// //   };
+// // }
+
 // import { ethers } from "ethers";
 // import { useState } from "react";
-// import { MetaMaskSDK } from "@metamask/sdk";
+// import { toast } from "react-toastify";
 
 // import { formatAddress } from "../helpers/formatAddress";
 
@@ -14,26 +83,35 @@
 //   const [provider, setProvider] = useState();
 
 //   async function requestAccount() {
-//     console.log("Hello");
-//     console.log(MetaMaskSDK);
-//     const sdk = new MetaMaskSDK({
-//       openDeeplink:
-//         "https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/",
-//     });
-
-//     const ethereum = sdk.getProvider();
-
-//     const test = new ethers.providers.WebBrowserProvider3Provider(ethereum);
-//     console.log("test:", test);
-
 //     if (window.ethereum) {
-//       // console.log("window.ethereum):", window.ethereum);
-//       // console.log("test:", test);
 //       const provider = new ethers.BrowserProvider(window.ethereum);
+//       console.log("provider:", provider);
 
 //       const signer = await provider.getSigner();
+//       console.log("signer:", signer);
+//       toast.info(`${JSON.stringify(signer)}`, {
+//         position: "top-center",
+//         autoClose: 3000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//         theme: "colored",
+//       });
 
+//       console.log("provider.getBalance:", provider.getBalance);
 //       const balance = await provider.getBalance(signer.address);
+//       toast.info(`${JSON.stringify(provider.getBalance)}`, {
+//         position: "top-center",
+//         autoClose: 3000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//         theme: "colored",
+//       });
 
 //       const formatted = formatAddress(signer.address);
 
@@ -42,7 +120,19 @@
 //       setSigner(signer);
 //       setProvider(provider);
 //     } else {
-//       alert("No crypto wallet found. Please install it.");
+//       // Якщо MetaMask не встановлено, відкрийте deep link.
+//       const deepLink = `https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/`;
+//       window.open(deepLink, "_blank");
+//       toast.info("deepLink", {
+//         position: "top-center",
+//         autoClose: 3000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//         theme: "colored",
+//       });
 //     }
 //   }
 
@@ -68,13 +158,10 @@
 // }
 
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 import { formatAddress } from "../helpers/formatAddress";
-
-// DEEPL LNK
-// https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/
 
 export function useEthereumAccount() {
   const [balance, setBalance] = useState(null);
@@ -82,59 +169,120 @@ export function useEthereumAccount() {
   const [signer, setSigner] = useState(null);
   const [provider, setProvider] = useState();
 
+  useEffect(() => {
+    // Fetch balance when the signer is available
+    async function fetchBalance() {
+      if (signer) {
+        const balance = await provider.getBalance(signer.address);
+        setBalance(ethers.formatEther(balance));
+      }
+    }
+    fetchBalance();
+  }, [provider, signer]);
+
   async function requestAccount() {
     if (window.ethereum) {
       const provider = new ethers.BrowserProvider(window.ethereum);
 
-      const signer = await provider.getSigner();
-      console.log("signer:", signer);
-      toast.info(`${JSON.stringify(signer)}`, {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+      try {
+        const signer = await provider.getSigner();
+        const balance = await provider.getBalance(signer.address);
 
-      const balance = await provider.getBalance(signer.address);
-      toast.info(ethers.formatEther(balance), {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+        const formatted = formatAddress(signer.address);
 
-      const formatted = formatAddress(signer.address);
+        setBalance(ethers.formatEther(balance));
+        setWalletAddress(formatted);
+        setSigner(signer);
+        setProvider(provider);
 
-      setBalance(ethers.formatEther(balance));
-      setWalletAddress(formatted);
-      setSigner(signer);
-      setProvider(provider);
+        toast.info(`Account connected successfully!`, {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      } catch (error) {
+        console.error("Error connecting account:", error);
+        toast.error(`Error connecting account: ${error.message}`, {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
     } else {
-      // Якщо MetaMask не встановлено, відкрийте deep link.
       const deepLink = `https://metamask.app.link/dapp/my-wallet-app-delta.vercel.app/`;
       window.open(deepLink, "_blank");
+      toast.info("Please install MetaMask to connect your account.", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
     }
   }
 
   async function transferTokens(receiverAddress, transferAmount) {
-    const tx = await signer.sendTransaction({
-      to: receiverAddress,
-      value: ethers.parseEther(transferAmount),
-    });
+    if (!signer) {
+      toast.error("Please connect your account first.", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      return;
+    }
 
-    await tx.wait();
+    try {
+      const tx = await signer.sendTransaction({
+        to: receiverAddress,
+        value: ethers.parseEther(transferAmount),
+      });
 
-    const balance = await provider.getBalance(signer.address);
+      await tx.wait();
 
-    setBalance(ethers.formatEther(balance));
+      const balance = await provider.getBalance(signer.address);
+
+      setBalance(ethers.formatEther(balance));
+      toast.success("Tokens transferred successfully!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } catch (error) {
+      console.error("Error transferring tokens:", error);
+      toast.error(`Error transferring tokens: ${error.message}`, {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
   }
 
   return {
